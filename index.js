f71436c84c03f72eb17b9cab768a8b93f3d09d58
AngelFmc <612110393@utsalamanca.edu.mx>
2023-02-15 09:58:18 -0600

    Segundo cambio

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const {initializeApp} = require('firebase/app')
-const {getFirestore} = require('firebase/firestore')
+const {getFirestore, collection , getDoc, doc, setDoc} = require('firebase/firestore')
 require('dotenv/config')
 
 // Configuracion de firebase
@@ -21,6 +21,68 @@ const firebaseConfig = {
   //Inicializar el servidor
   const app = express()
 
+  app.use(express.json())
+
+  //Ruta para las peticiones
+  app.post('/registro', (req, res) =>{
+    const{ name, lastname, email, password, number} = req.body
+
+    //Validaciones de los datos
+    if(name.length <= 3){
+      res.json({
+        'alert': 'Nombre requiere minimo 3 caracters'
+      })
+    }else if (lastname.length <3){
+      res.json({
+        'alert': 'Apellido requiere minimo 3 caracteres'
+      })
+    }else if (!email.length){
+      res.json({
+        'alert': 'Debes escribir correo electronico'
+      })
+    }else if (password.length < 8){
+      res.json({
+        'alert': 'Password requiere minimo 8 caracteres'
+      })
+    }
+    else if (!Number(number) || number.length <10){
+      res.json({
+        'alert': 'Introduce un numero de telefono correcto'
+      })
+    } else {
+      const users = collection(db, 'users')
+
+      getDoc(doc(users,email)).then(user =>{
+        if(user.exists()){
+          res.json({
+            'alert': 'El correo ya existe'
+          })
+        }else{
+          bcrypt.genSalt(10, (err, salt) =>{
+            bcrypt.hash(password, salt, (err, hash) =>{
+              req.body.password = hash
+
+              //Guardar en la base de datos
+              setDoc(doc(users, email), req.body).then(() =>{
+                res.json({
+                  'alert': 'success'
+                })
+              })
+            })
+          })
+        }
+      })
+    }
+  })
+
+   app.get('/usuarios', (req, res) =>{
+    const users = collection(db, 'users')
+    console.log('Usuarios', users)
+    res.json({
+      'alert': 'success',
+      users
+    })
+   })
   const PORT = process.env.PORT||19000
 
   //ejecutamos el servidor
