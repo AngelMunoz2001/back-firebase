b587eb025aada0902dbb25cc2fd25a340ae36b92
Bryan Coronado <612110528@utsalamanca.edu.mx>
2023-02-21 12:47:08 -0600

    Back

--- index.js
+++ index.js
@@ -75,14 +75,55 @@ const firebaseConfig = {
     }
   })
 
-   app.get('/usuarios', (req, res) =>{
-    const users = collection(db, 'users')
-    console.log('Usuarios', users)
+   app.get('/usuarios', async (req, res) =>{
+    const colRef  = collection(db, 'users')
+    const docsSnap = await getDocs(colRef)
+    let data = []
+    docsSnap.forEach(doc =>{
+      data.push(doc.data())
+    })
     res.json({
       'alert': 'success',
-      users
+      data
     })
    })
+
+   app.post('/login', async(req,res) =>{
+    let {email, password} = req.body
+
+    if(!email.length || !pass.length){
+      return res.json({
+        'alert': 'no se han recibido los datos correctamente'
+      })
+     }
+
+  const users = collection(db, 'users')
+  getDoc(doc(users, email))
+  .then(user =>{
+      if(!user.exists()){
+        return res.json({
+          'alert': 'Correo no registrado en la base de datos'
+        })
+      }else{
+        bcrypt.compare(password, user.data().password, (error, result)=>{
+          if(result){
+            let data = user.data()
+            res.json({
+              'alert': 'Success',
+              name: data.name,
+              email: data.email
+            })
+          }else{
+            return res.json({
+              'alert': 'Password Inncorrecto'
+            })
+          }
+        })
+      }
+   })
+  })
+    
+   
   const PORT = process.env.PORT||19000
 
   //ejecutamos el servidor
